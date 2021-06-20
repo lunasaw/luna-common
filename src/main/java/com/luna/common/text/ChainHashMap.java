7068422493a407080fa1697c34ef360c67cf6ec3
luna <iszychen@gmail.com>
2021-06-20 23:15:25 +0800

    :fire: upgrade version 1.3.7 add chainMap

---
+++ src/main/java/com/luna/common/text/ChainHashMap.java
@@ -0,0 +1,46 @@
+package com.luna.common.text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+/**
+ * @author luna
+ */
+public class ChainHashMap<K, V> extends HashMap<K, V> {
+
+    public ChainHashMap(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public ChainHashMap() {
+        super();
+    }
+
+    public ChainHashMap(Map<? extends K, ? extends V> m) {
+        super(m);
+    }
+
+    public ChainHashMap(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    public static <K extends Comparable, V> ChainHashMap<K, V> newChainMap() {
+        return new ChainHashMap<>();
+    }
+
+    /**
+     * 对 HashMap 的 put() 的方法进行封转返回 ChainHashMap 来实现 链式添加
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    public ChainHashMap<K, V> putIfNull(K key, V value) {
+        if (ObjectUtils.isNotEmpty(key) && ObjectUtils.isNotEmpty(value)) {
+            this.put(key, value);
+        }
+        return this;
+    }
+}
---
+++ src/main/java/com/luna/common/text/ChainTreeMap.java
@@ -0,0 +1,48 @@
+package com.luna.common.text;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+/**
+ * @author luna
+ */
+public class ChainTreeMap<K, V> extends TreeMap<K, V> {
+
+    public ChainTreeMap() {
+        super();
+    }
+
+    public ChainTreeMap(Comparator<? super K> comparator) {
+        super(comparator);
+    }
+
+    public ChainTreeMap(Map<? extends K, ? extends V> m) {
+        super(m);
+    }
+
+    public ChainTreeMap(SortedMap<K, ? extends V> m) {
+        super(m);
+    }
+
+    public static <K extends Comparable, V> ChainTreeMap<K, V> newChainMap() {
+        return new ChainTreeMap<>();
+    }
+
+    /**
+     * 对 HashMap 的 put() 的方法进行封转返回 ChainHashMap 来实现 链式添加
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    public ChainTreeMap<K, V> putIfNotEmpty(K key, V value) {
+        if (ObjectUtils.isNotEmpty(key) && ObjectUtils.isNotEmpty(value)) {
+            this.put(key, value);
+        }
+        return this;
+    }
+}
