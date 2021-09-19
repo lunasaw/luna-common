7b209263e8c205b522e00625f82d68467d0961b9
luna <iszychen@gmail.com>
2021-09-19 20:58:59 +0800

    :fire: upgrade version 1.6.2-RELEASE upgrade os info

--- src/main/java/com/luna/common/constant/Constant.java
+++ src/main/java/com/luna/common/constant/Constant.java
@@ -6,167 +6,177 @@ package com.luna.common.constant;
  */
 public interface Constant {
     /** 0.0 */
-    String    ONE_POINT_ONE                = "0.0";
+    String ONE_POINT_ONE                = "0.0";
     /** 0. */
-    String    ONE_POINT                    = "0.";
+    String ONE_POINT                    = "0.";
 
     /**
      * 加入成功
      */
-    String    AUDIT_SUCCESS                = "success";
+    String AUDIT_SUCCESS                = "success";
     /**
      * 审核中
      */
-    String    AUDIT_PENDING                = "pending";
+    String AUDIT_PENDING                = "pending";
     /**
      * 待审核
      */
-    String    AUDIT_WAITING                = "waiting";
+    String AUDIT_WAITING                = "waiting";
     /**
      * 审核拒绝
      */
-    String    AUDIT_REJECT                 = "reject";
+    String AUDIT_REJECT                 = "reject";
 
     /**
      * 数字常量 0
      */
-    int       NUMBER_ZERO                  = 0;
+    int    NUMBER_ZERO                  = 0;
 
     /**
      * 数字常量 1
      */
-    int       NUMBER_ONE                   = 1;
+    int    NUMBER_ONE                   = 1;
 
     /**
      * 数字常量 2
      */
-    int       NUMBER_TWO                   = 2;
+    int    NUMBER_TWO                   = 2;
 
     /**
      * 数字常量 3
      */
-    int       NUMBER_THREE                 = 3;
+    int    NUMBER_THREE                 = 3;
 
     /**
      * 数字常量 4
      */
-    int       NUMBER_FOUR                  = 4;
+    int    NUMBER_FOUR                  = 4;
 
     /**
      * 数字常量 5
      */
-    int       NUMBER_FIVE                  = 5;
+    int    NUMBER_FIVE                  = 5;
 
     /**
      * 数字常量 6
      */
-    int       NUMBER_SIX                   = 6;
+    int    NUMBER_SIX                   = 6;
 
     /**
      * 数字常量 7
      */
-    int       NUMBER_SEVEN                 = 7;
+    int    NUMBER_SEVEN                 = 7;
 
     /**
      * 数字常量 8
      */
-    int       NUMBER_EIGHT                 = 8;
+    int    NUMBER_EIGHT                 = 8;
 
     /**
      * 数字常量 9
      */
-    int       NUMBER_NINE                  = 9;
+    int    NUMBER_NINE                  = 9;
 
     /**
      * 数字常量 10
      */
-    int       NUMBER_TEN                   = 10;
+    int    NUMBER_TEN                   = 10;
 
     /**
      * 数字常量 11
      */
-    int       NUMBER_ELEVEN                = 11;
+    int    NUMBER_ELEVEN                = 11;
 
     /**
      * 数字常量 12
      */
-    int       NUMBER_TWELVE                = 12;
+    int    NUMBER_TWELVE                = 12;
 
     /**
      * 数字常量 20
      */
-    int       NUMBER_TWENTY                = 20;
+    int    NUMBER_TWENTY                = 20;
 
     /**
      * 数字常量 28
      */
-    int       NUMBER_TWENTY_EIGHT          = 28;
+    int    NUMBER_TWENTY_EIGHT          = 28;
 
     /**
      * 数字常量 29
      */
-    int       NUMBER_TWENTY_NINE           = 29;
+    int    NUMBER_TWENTY_NINE           = 29;
 
     /**
      * 数字常量 30
      */
-    int       NUMBER_THIRTY                = 30;
+    int    NUMBER_THIRTY                = 30;
 
     /**
      * 数字常量 31
      */
-    int       NUMBER_THIRTY_ONE            = 31;
+    int    NUMBER_THIRTY_ONE            = 31;
 
     /**
      * 数字常量 40
      */
-    int       NUMBER_FOUTY                 = 40;
+    int    NUMBER_FOUTY                 = 40;
 
     /**
      * 数字常量 50
      */
-    int       NUMBER_FIFTY                 = 50;
+    int    NUMBER_FIFTY                 = 50;
 
     /**
      * 数字常量 60
      */
-    int       NUMBER_SIXTY                 = 60;
+    int    NUMBER_SIXTY                 = 60;
 
     /**
      * 数字常量 70
      */
-    int       NUMBER_SEVENTY               = 70;
+    int    NUMBER_SEVENTY               = 70;
 
     /**
      * 数字常量 80
      */
-    int       NUMBER_EIGHTY                = 80;
+    int    NUMBER_EIGHTY                = 80;
 
     /**
      * 数字常量 90
      */
-    int       NUMBER_NINETY                = 90;
+    int    NUMBER_NINETY                = 90;
 
     /**
      * 数字常量 100
      */
-    int       NUMBER_HUNDERD               = 100;
+    int    NUMBER_HUNDERD               = 100;
 
     /**
      * 常量360F
      */
-    float     NUMBER_THREE_HUNDRED_AND_SIX = 360F;
+    float  NUMBER_THREE_HUNDRED_AND_SIX = 360F;
 
     /**
      * 常量 5000
      */
-    int       FIVE_THOUSAND                = 5000;
+    int    FIVE_THOUSAND                = 5000;
+
+    /**
+     * 常量 -1
+     */
+    int    NEGATIVE_ONE                 = -1;
+
+    /**
+     * 常量 -10
+     */
+    int    NEGATIVE_TEN                 = -10;
 
     /** 随机字符串 */
-    String    SYMBOLS                      = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String    TABLE                        =
+    String SYMBOLS                      = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String TABLE                        =
         "0123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz!@#$";
-    String    TABLE_TWO                    =
+    String TABLE_TWO                    =
         "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz!@#$^&*<>/.,";
 
 }
