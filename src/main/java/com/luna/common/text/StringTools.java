a00d5d96266ea998b297690dcc482ea0b45b09da
chenzhangyue <15696756582@163.com>
2023-03-06 14:49:31 +0800

    :rocket: 2.2.2

--- src/main/java/com/luna/common/text/StringTools.java
+++ src/main/java/com/luna/common/text/StringTools.java
@@ -851,29 +851,6 @@ public class StringTools extends StringUtils {
     }
 
     /**
-     * 格式化文本, {} 表示占位符<br>
-     * 此方法只是简单将占位符 {} 按照顺序替换为参数<br>
-     * 如果想输出 {} 使用 \\转义 { 即可，如果想输出 {} 之前的 \ 使用双转义符 \\\\ 即可<br>
-     * 例：<br>
-     * 通常使用：format("this is {} for {}", "a", "b") =》 this is a for b<br>
-     * 转义{}： format("this is \\{} for {}", "a", "b") =》 this is \{} for a<br>
-     * 转义\： format("this is \\\\{} for {}", "a", "b") =》 this is \a for b<br>
-     *
-     * @param template 文本模板，被替换的部分用 {} 表示，如果模板为null，返回"null"
-     * @param params   参数值
-     * @return 格式化后的文本，如果模板为null，返回"null"
-     */
-    public static String format(CharSequence template, Object... params) {
-        if (null == template) {
-            return null;
-        }
-        if (ObjectUtils.isEmpty(params) || isBlank(template)) {
-            return template.toString();
-        }
-        return StrFormatter.format(template.toString(), params);
-    }
-
-    /**
      * 格式化文本，使用 {varName} 占位<br>
      * map = {a: "aValue", b: "bValue"} format("{a} and {b}", map) ---=》 aValue and bValue
      *
@@ -931,6 +908,29 @@ public class StringTools extends StringUtils {
             template2 = replace(template2, start + entry.getKey() + end, value);
         }
         return template2;
+    }
+
+    /**
+     * 格式化文本, {} 表示占位符<br>
+     * 此方法只是简单将占位符 {} 按照顺序替换为参数<br>
+     * 如果想输出 {} 使用 \\转义 { 即可，如果想输出 {} 之前的 \ 使用双转义符 \\\\ 即可<br>
+     * 例：<br>
+     * 通常使用：format("this is {} for {}", "a", "b") =》 this is a for b<br>
+     * 转义{}： format("this is \\{} for {}", "a", "b") =》 this is \{} for a<br>
+     * 转义\： format("this is \\\\{} for {}", "a", "b") =》 this is \a for b<br>
+     *
+     * @param template 文本模板，被替换的部分用 {} 表示，如果模板为null，返回"null"
+     * @param params   参数值
+     * @return 格式化后的文本，如果模板为null，返回"null"
+     */
+    public static String format(CharSequence template, Object... params) {
+        if (null == template) {
+            return null;
+        }
+        if (ObjectUtils.isEmpty(params) || isBlank(template)) {
+            return template.toString();
+        }
+        return StrFormatter.format(template.toString(), params);
     }
 
     /**
