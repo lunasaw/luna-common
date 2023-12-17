3a16e9ac6f80b792ddeb0209cb61b95651b7ec43
chenzhangyue <15696756582@163.com>
2023-12-17 19:30:24 +0800

    :bookmark: 2.5.4 精确计算

--- src/main/java/com/luna/common/media/MimeTypeConstant.java
+++ src/main/java/com/luna/common/media/MimeTypeConstant.java
@@ -3,7 +3,7 @@ package com.luna.common.media;
 /**
  * 媒体类型工具类
  * 
- * @author ruoyi
+ * @author luna
  */
 public class MimeTypeConstant {
     public static final String   IMAGE_PNG                 = "image/png";
