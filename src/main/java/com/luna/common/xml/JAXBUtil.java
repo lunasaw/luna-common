3aad8a604a3580f38a53d350daf90eb8fbd63686
chenzhangyue <15696756582@163.com>
2024-04-10 17:40:40 +0800

    :bookmark: 版本升级xml, javax.xml.bind.-> jakarta.xml.bind.

--- src/main/java/com/luna/common/xml/JAXBUtil.java
+++ src/main/java/com/luna/common/xml/JAXBUtil.java
@@ -5,13 +5,13 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
 
 import com.luna.common.file.FileTools;
 import com.luna.common.io.IoUtil;
--- src/main/java/com/luna/common/xml/XmlUtil.java
+++ src/main/java/com/luna/common/xml/XmlUtil.java
@@ -5,8 +5,8 @@ import java.io.*;
 import java.util.*;
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Unmarshaller;
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.*;
